ead502b0275c388283998ac6160982379e6120d7
Rafael Santos <rafaelsantosdearaujo@gmail.com>
2021-05-29 14:54:53 -0300

    Tratamento da entrada do usuario , Maisculo e Minusculo e Espaços

--- forca.py
+++ forca.py
@@ -10,10 +10,11 @@ def jogar():
     while(not enforcou and not acertou ):
 
         chute = input("Qual letra ?")
+        chute = chute.strip()
         print("...")
         index = 0
         for letra in palavra_secreta:
-            if(chute == letra):
+            if(chute.upper() == letra.upper()):
                 print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index +1
     print("Fim do jogo")
