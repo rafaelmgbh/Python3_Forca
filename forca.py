59dfe34bf10665461c9ca212f5fecaf3303ed372
Rafael Santos <rafaelsantosdearaujo@gmail.com>
2021-06-01 20:45:38 -0300

    Melhorado o tratamento da entrada do usuario , e adicionado mensagem quando acerta ou erra todas as tentativas

--- forca.py
+++ forca.py
@@ -1,22 +1,35 @@
 def jogar():
     print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
+    print("*** Bem vindo ao jogo da Forca!*** ")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     acertou = False
+    erros = 0
 
     while(not enforcou and not acertou ):
 
         chute = input("Qual letra ?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
         print("...")
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index +1
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+    if(acertou):
+        print("Voce ganhou !!!")
+    else:
+        print("Voce perdeu !!!")
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
